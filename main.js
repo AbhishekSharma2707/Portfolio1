35815719ff4a1b862c2a038bdf83139a3426291a
AbhishekSharma2707 <abhisheksharma27072002@gmail.com>
2024-05-06 12:29:44 +0200

    changes

---
+++ main.js
@@ -0,0 +1,25 @@
+// const typed = new Typed('.text', {
+//     string:["Frontend Developer", "Youtuber", "Web Developer"] ,
+//     typeSpeed:100,
+//     backSpeed:100,
+//     backDelay:1000,
+//     loop:true
+// });
+var typed = new Typed('#element', {
+    strings:["Frontend Developer", "Full Stack Developer", "Web Developer"] ,
+    
+    typeSpeed: 100,
+    backSpeed:100,
+  backDelay:1000,
+    loop:true
+  });
+
+//   function compare()
+// {
+//     int num=2;
+//     char b=2;
+//     if(a==b)
+//         return true;
+//     else
+//         return false;
+// }
